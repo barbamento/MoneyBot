8a7ba773ddb55cd1871626a517abb30a0a89bfad
barbamento <72825350+barbamento@users.noreply.github.com>
2022-01-04 12:19:54 +0100

    add multiindex

--- wallet.py
+++ wallet.py
@@ -7,12 +7,13 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import datetime
 from binance import Client
+from requests.models import codes
 
 #   TO DO LIST
 #       
 #       ADD MULTI INDEX
 #
-
+#       ADD SUPPORT MULTIWALLET FOR CRYPTO
 
 class wallet:
     def __init__(self,exchanges,apis,path="./assets"):
@@ -34,14 +35,13 @@ class wallet:
         self.path=path
         self.key_inizialization(exchanges,apis)
         self.wallet=self.wallet_inizialitazion()
-        print(self.wallet)
         #if str(datetime.date.today()) not in np.unique(self.wallet["date"].to_list()):
         if True:
             if "binance" in exchanges:
                 self.add_today_binance()
             if "nexo" in exchanges:
                 self.add_today_nexo()
-        self.check_missing_days("binance")
+        #self.check_missing_days("binance")
         self.wallet.to_csv(os.path.join(self.path,"wallet.csv"))
 
     def key_inizialization(self,exchanges,apis):
@@ -54,9 +54,9 @@ class wallet:
         """
         create a new wallet if none are found in the directory
         """
-        wallet=pd.DataFrame(columns=["paid","cost","not paid","value","location"])
-        index=pd.MultiIndex(names=["asset","date"])
-        wallet.index=index
+        wallet=pd.DataFrame(columns=["asset","date","location","ammount","value"])
+        wallet=wallet.set_index(["asset","date","location"])
+        wallet=wallet.sort_index()
         return wallet
 
     def wallet_inizialitazion(self):
@@ -66,22 +66,41 @@ class wallet:
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         try:
-            wallet=pd.read_csv(os.path.join(self.path,"wallet.csv"),index_col=0)
+            wallet=pd.read_csv(os.path.join(self.path,"wallet.csv"),index_col=[0,2])
         except:
             wallet=self.wallet_creation()
             wallet.to_csv(os.path.join(self.path,"wallet.csv"))
         return wallet
       
-    def binance_data(self):#find a way to secret the keys
+    def binance_data(self):#add locking stacking, maybe with external function
         """
         Get data from binance api
+
+            return:
+        pd.Dataframe, with multiindex ["asset","date"] and a single entries called ["ammount"].
         """
         client = Client(self.api_key, self.api_secret)
-        df=pd.DataFrame(client.get_account()["balances"])
-        df=df.set_index(["asset"])
-        df=pd.DataFrame(df.to_numpy().astype(np.float64),columns=df.columns,index=df.index)
+        df=pd.DataFrame(client.get_account()["balances"]).astype({"asset":str,"free":float,"locked":float})
+        df=df.set_index("asset")
         df=df.loc[(df!=0).any(1)]
-        return df
+        locked=df[df.index.str[:2]=="LD"].apply(sum,axis=1)
+        locked.index=locked.index.str[2:]
+        free=df[df.index.str[:2]!="LD"].apply(sum,axis=1)
+        assets=list(set(locked.index).union(set(free.index)))
+        index=pd.MultiIndex.from_arrays([assets,[datetime.date.today()]*len(assets),["binance"]*len(assets)],names=["asset","date","location"])
+        result=pd.DataFrame(columns=["ammount"],index=index)
+        for i in assets:
+            if i in locked.index:
+                v1=locked.loc[i]
+            else:
+                v1=0
+            if i in free.index:
+                v2=free.loc[i]   
+            else:
+                v2=0
+            result.loc[(i,str(datetime.date.today()),"binance"),"ammount"]=v1+v2#performance error here
+        result.sort_index()
+        return result
 
     def add_today_binance(self):
         """
@@ -90,32 +109,18 @@ class wallet:
         """
         cmc=coinmarketcapapi.CoinMarketCapAPI(self.cmc_key)
         date=datetime.date.today()
-        yesterday=date-datetime.timedelta(days=1)
         today_df=self.binance_data()
-        print(today_df)
-        if self.wallet.empty:
-            wallet=self.wallet
-        else:
-            wallet=self.wallet[self.wallet["location"]=="binance"]
-        temp_df=pd.DataFrame(columns=wallet.columns)
-        df=today_df.apply(np.sum,axis=1)
+        temp_df=pd.DataFrame(columns=self.wallet.columns,index=today_df.index)
+        temp_df.sort_index()
+        prices=[]
         for asset in today_df.index:
-            temp_df.loc[asset,"date"]=date
-            try:
-                current_value=wallet[wallet["date"]==yesterday].loc[asset,"not paid"]+wallet[wallet["date"]==yesterday].loc[asset,"paid"]
-            except:
-                current_value=0
-            name= asset if asset[:2]!="LD" else asset[2:]
-            print(name)
-            response=cmc.cryptocurrency_quotes_latest(symbol=name,convert="EUR").__str__().replace("\'", "\"").replace('None','"None"').split("OK: ")[1]
-            convertion_rate=json.loads(response)[name]["quote"]["EUR"]["price"]
-            temp_df.loc[asset,["not paid","value","location"]]=[df[asset]-current_value,
-                                                            (df[asset]-current_value)*convertion_rate ,
-                                                            "binance"]
-        if date not in np.unique(self.wallet["date"]):
-            self.wallet=self.wallet.append(temp_df.fillna(0))
-            self.check_missing_days("binance")
-        return temp_df.fillna(0)
+            response=cmc.cryptocurrency_quotes_latest(symbol=asset[0],convert="EUR").__str__().replace("\'", "\"").replace('None','"None"').split("OK: ")[1]
+            prices+=[json.loads(response)[asset[0]]["quote"]["EUR"]["price"]]
+        today_df["value"]=np.array(today_df["ammount"].to_list())*np.array(prices)
+        print(self.wallet.index.to_list())
+        if str(date) not in np.unique(self.wallet.index.to_list()):
+            self.wallet=self.wallet.append(today_df)
+        return temp_df
 
     def check_missing_days(self,exchange):
         wallet=self.wallet[self.wallet["location"]==exchange]
